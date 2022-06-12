8a085a08903aac806ff7eeb2a67aaf2b3c17dae1
arugenre <200103185@stu.sdu.edu.kz>
2022-06-12 21:10:46 +0600

    changes

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-import chalk from 'chalk';
-console.log(chalk.green('Incubator'));
-console.warn('Incubator blablalbla');
+function lightModeOn(){
+    const root = document.getElementById("body");
+    root.className = "light";
+    localStorage.setItem("theme", "light");
+}
+function darkModeOn(){
+    const root = document.getElementById("body");
+    root.className = "dark";
+    localStorage.setItem("theme", "dark");  
+}
+function getValue(){
+    const theme = localStorage.getItem("theme");
+    const root = document.getElementById("body");
+    root.className = theme;
+    if (theme=="dark"){
+        $("#switch").attr("checked", "checked");
+    }
+}
+function check(){
+    if($('#switch').is(':checked')){
+        darkModeOn();
+    } else {
+        lightModeOn();
+    }
+} 
+function setCookie() {
+    const input = $("#input").val();
+    console.log(input);
+    document.cookie = `user=${input}; expires=Wed, 15 Jun 2022 00:00:00 GMT`;
+}
